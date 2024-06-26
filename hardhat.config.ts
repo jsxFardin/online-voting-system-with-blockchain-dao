dfd38b42518e1ced9faffc62928e0b610a9fa2ae
fardin.ahsan75@gmail.com <fardin.ahsan75@gmail.com>
2024-06-27 00:32:37 +0600

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+};
+
+export default config;
---
+++ readme.md
@@ -0,0 +1,38 @@
+## Online Voting System Using Blockchain(DAOs)
+
+A Comprehensive Analysis and Implementation of DAOs (Decentralized Autonomous Organization) as Democratic Agents
+
+1. Install npm packages
+
+```
+$ yarn install or npm i
+```
+
+2. To deploy 
+
+```
+$ yarn hardhat deploy
+```
+
+3. To run application 
+
+```
+$ yarn hardhat node
+```
+
+4. To create a proposal 
+
+```
+$  yarn hardhat run scripts/propose.ts --network localhost
+```
+
+5. For votng 
+```
+$ yarn hardhat run scripts/vote.ts --network localhost
+```
+
+5. For executeing the proposal
+```
+$ yarn hardhat run scripts/queue-and-execute.ts --network localhost
+```
+
